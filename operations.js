96e5d6ae420ea196ff5ab5adae4673c7e46fc41a
Ana Paula Hiraiwa Montanher <hhiraiwa1@hotmail.com>
2025-01-06 10:51:44 -0300

    first commit

---
+++ operations.js
@@ -0,0 +1,17 @@
+function sum(soma,numeros){
+    soma=0;
+    numeros=[];
+
+
+    return value1+value2;
+}
+
+function subtraction(value1,value2){
+    return value1-value2;
+}
+function plus(value1,value2){
+    return value1*value2;
+}
+function division(value1,value2){
+    return value1/value2;
+}
