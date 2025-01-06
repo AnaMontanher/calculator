b033980e63bd0585422d08d763d2d30b734b2db9
Ana Paula Hiraiwa <hhiraiwa1@hotmail.com>
2025-01-06 11:41:43 -0300

    remove links and adding button

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
+
+const calculator = document.querySelector(".buttons");
+const keys = calculator.querySelector(".buttons")
 const equalButton = document.querySelector(".btn-equal");
 const numberButton = document.querySelector(".btn-number");
 const operatorButton = document.querySelector(".btn-operator");
 const result = document.querySelector(".display");
 
-const resultadoConta = 
-
-numberButton.addEventListener("click", function (number){
+const resultadoConta = numberButton.addEventListener(
+  "click",
+  function (number) {
     number.preventDefault();
     result.textContent = numberButton;
-
-} 
-)
-
-
+  }
+);
 
 equalButton.addEventListener("click", function (final) {
-    final.preventDefault();
+  final.preventDefault();
 
-
- result.textContent = resultadoConta;
-
+  result.textContent = resultadoConta;
 });
